be90de306138bef4f195127b96be862bf60e802e
Anton Monjon <monjon.an@gmail.com>
2015-11-14 19:00:53 +0100

    Adding scoring and health bar system + HUD

--- src/Eggplant Revenge/Assets/Scripts/GameController.cs
+++ src/Eggplant Revenge/Assets/Scripts/GameController.cs
@@ -6,9 +6,12 @@ public class GameController : MonoBehaviour {
 	public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
 	
+	public static int score;
+	
 	// Use this for initialization
 	void Start () {
 		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+		score = 0;
 	}
 	
 	// Update is called once per frame
