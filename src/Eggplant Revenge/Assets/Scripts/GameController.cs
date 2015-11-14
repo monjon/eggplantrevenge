df0726db951751841cd11aa8f2776d41ee27120d
Anton Monjon <monjon.an@gmail.com>
2015-11-14 11:03:48 +0100

    Adding Kaiju 1st draft model and movement using left and right keys

--- src/Eggplant Revenge/Assets/Scripts/GameController.cs
+++ src/Eggplant Revenge/Assets/Scripts/GameController.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject KaijuObject;
-
 	// Use this for initialization
 	void Start () {
 	
@@ -12,19 +10,6 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.LeftArrow)){
-			//  Debug.Log("toto");
-			
-			Vector3 position = KaijuObject.transform.position;
-			position.x--;
-			KaijuObject.transform.position = position;
-		}
-		
-		if (Input.GetKeyDown(KeyCode.RightArrow))
-		{
-				Vector3 position = KaijuObject.transform.position;
-				position.x++;
-				KaijuObject.transform.position = position;
-		}
+
 	}
 }
