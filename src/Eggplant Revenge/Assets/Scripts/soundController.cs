571771f2524ec5e2ccdfa8f618c9264da8471b03
yendhi <newdhidhi@gmail.com>
2015-11-15 15:39:28 +0100

    soud

---
+++ src/Eggplant Revenge/Assets/Scripts/soundController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soundController : MonoBehaviour {
+	public AudioClip monsterExplosion;
+	public AudioClip monsterWalk1;
+	public AudioClip monsterWalk2;
+	public AudioClip laser;
+	public AudioClip monsterCry1;
+	public AudioClip monsterCry2;
+	public AudioClip monsterCry3;
+	public AudioClip helicoMove;
+	public AudioClip helicoExplosion;
+	public AudioClip helicoFall;
+	public AudioClip helicoFire;
+	public AudioClip tankMove;
+	public AudioClip tankExplosion;
+	public AudioClip tankStop;
+	public AudioClip tankReady;
+	public AudioClip tankFire;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
